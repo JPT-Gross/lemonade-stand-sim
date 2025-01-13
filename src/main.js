9660f649404e9b2e2615106e30c2eaeb51ea579c
Matthew Gross <91265803+JPT-Gross@users.noreply.github.com>
2025-01-13 12:27:34 -0500

    Add buttonState method

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
-const tickRate = 1000 / 30;
+const tickRate = 1000 / 30; // 30 FPS
 let score = 0;
 
 let juicer = new Building('Juicer', 0.1, 15, 'buyJuicer');
-
-function incScore() {
-    score += juicer.cps;
-}
+let sugar = new Building('Sugar', 1, 100, 'buySugar');
 
 function scorePlusPlus() {
     score++;
 }
 
+function incScore() {
+    score += juicer.cps;
+    score += sugar.cps;
+}
+
+function updateButtons() {
+    juicer.buttonState();
+    sugar.buttonState();
+}
+
 function updatePage() {
     incScore();
+    updateButtons();
     document.getElementById('score').innerHTML =
         '$' + Math.floor(score).toLocaleString();
 }
